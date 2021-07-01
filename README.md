355a6057dc76ee149c072a5731fbb4edc6c6d90e
xxyangyoulin <xxyangyoulin@outlook.com>
2021-07-01 23:04:55 +0800

    add juhe-weather

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # polybar_easy_script
-Collection of personal gadgets and scripts！polybar状态栏的个人小工具、脚本集合！
+
+polybar状态栏的个人小工具、脚本集合！
+
+### 使用提醒
+
+使用本脚本需在polybar启动前加入`$EASY_SH_PATH`变量，**指定脚本所在位置**。
+
+并且在该位置下建立三个目录：`cache`、`log`、`polybar_easy_script`三个文件夹。`polybar_easy_script`文件夹即为本项目的`clone`。
+
+> 或者手动修改脚本中的文件路径。
+
+---
+
+### 聚合天气（juhe.cn）
+
+openweathermap虽然免费并且差不多可以无限调用，但是在中国境内的精度堪忧。我发现国内聚合数据（juhe.cn）也提供得有免费方案，不过不是无限制的，每天免费100次。
+
+本脚本已加入调用限制，避免`polybar`的重启或者`interval =`的设置过短导致调用过度！放心使用。
+
+**1. 到juhe.cn注册并在`juhe-weather.sh`中添加KEY和支持的城市名**
+
+```js
+KEY=""
+CITY="清镇"
+```
+
+**2. polybar配置**
+
+```sh
+[module/juhe-weather]
+type = custom/script
+exec = $EASY_SH_PATH/polybar_easy_script/juhe-weather.sh
+interval = 15
+label-font = 3
+```
+
---
+++ juhe-weather.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+LOG=$EASY_SH_PATH/log/juhe-weather.log;
+CACHE=$EASY_SH_PATH/cache/juhe-weather.cache;
+
+read last_time info wid temp < $CACHE;
+
+echoinfo(){
+    echo "$1 $2°C"
+}
+
+echostale(){
+    now_time=$(date +%s)
+    if [ -n "$last_time" ] && [ $(($now_time-$last_time)) -lt 10800 ];then
+        echoinfo $info $temp
+    else
+        echoinfo "~"$info $temp
+    fi
+}
+
+if [ -n "$last_time" -a ! "$info" = "null" ];then
+    now_time=$(date +%s)
+    if [ $(($now_time-$last_time)) -lt 900  ];then
+        echoinfo $info $temp
+        return;
+    fi
+fi
+
+API="http://apis.juhe.cn/simpleWeather/query"
+KEY=""
+CITY="清镇"
+
+weather=$(curl -sf "$API?key=$KEY&city=$CITY")
+
+if [ -n "$weather" ]; then
+    weather_info=$(echo "$weather" | jq -r ".result.realtime.info")
+    weather_wid=$(echo "$weather" | jq -r ".result.realtime.wid")
+    weather_temp=$(echo "$weather" | jq -r ".result.realtime.temperature")
+
+    if [ "$weather_info" = "null" ];then
+        # 获取天气失败，使用旧的数据,超过3小时未更新，则加上～前缀
+        echostale;
+    else
+        echoinfo $weather_info $weather_temp
+        
+        echo $(date +%s) $weather_info $weather_wid $weather_temp > $CACHE
+    fi
+
+    echo "$(date)" "更新; 城市：$CITY 天气：$weather" > $LOG
+else
+    echostale;
+fi
